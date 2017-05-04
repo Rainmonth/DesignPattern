7340a8776fd319f69713139aa8540652c8a3823a
Randy <rainmonth@yeah.net>
2017-05-04 21:47:06 +0800

    add file

--- src/com/rainmonth/pattern/creational/singleton/AccountClient.java
+++ src/com/rainmonth/pattern/creational/singleton/AccountClient.java
@@ -1,5 +1,8 @@
 package com.rainmonth.pattern.creational.singleton;
 
+/**
+ * @author Randy Zhang
+ */
 public class AccountClient {
     public static void main(String[] args) {
         ATM atmKH = new ATM();
@@ -9,5 +12,7 @@ public class AccountClient {
         atmTP.withdraw(5000);
 
         // TODO: 2017/3/20 模拟出线程不安全的情况
+
+        // TODO: 2017/3/20 利用反射机制来创建出单例类的对象
     }
 }
